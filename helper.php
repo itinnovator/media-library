4a1cc205049fb058a651172a237afa83c46c0a35
itinnovator <jigeshraval89@gmail.com>
2017-12-31 12:39:57 +0530

    Media Generator function

--- helper.php
+++ helper.php
@@ -98,3 +98,69 @@ function getAdminTemplate($view, $data = array(), $print = false)
 
     return $html;
 }
+
+function generateMedia($id, $mediaList, $type = 'image', $var_type = 'string')
+{
+    if (!isset($mediaList)) {
+      return;
+    }
+
+    $ids = '';
+
+    if (count($mediaList) > 1) {
+      foreach ($mediaList as $key => $media) {
+        $media_opt = $media;
+        if (!$media_opt) {
+          continue;
+        }
+
+        if ($ids) {
+          $ids .= ','.$media_opt->id;
+        } else {
+          $ids = ''.$media_opt->id;
+        }
+        $html = generateMediaHTML($id, $media_opt);
+      }
+    } else {
+      $ids = $mediaList->id;
+      $html = generateMediaHTML($id, $mediaList);
+    }
+
+    $name = $id;
+    if ($var_type == 'array') {
+      $name = $id.'[]';
+    }
+
+    if ($type != 'video'){
+      $html .='<input type="hidden" id="' .$id. '" name="' .$name. '" value="' .$ids. '">';
+    }
+
+    $html .='</div>';
+
+    return $html;
+}
+
+function generateMediaHTML($id, $media_opt)
+{
+    $type = $media_opt->type;
+    $html = '<div id="'.$id.'_wrapper" class="preview-wrapper">';
+    if ($type == 'image') {
+      $html .='<div class="selected-img-preview"> <img id_media="'.$media_opt->id.'" src="'.getMedia($media_opt->name, '150, 150').'" class="full select-library-img"> <div class="img-action"> <button type="button" class="btn btn-success" id="edit-media-img" id_media="'.$media_opt->id.'">Edit</button> <button type="button" class="btn btn-danger" id="delete-media-img" id_media="'.$media_opt->id.'">Delete</button>
+      </div></div>';
+
+    } elseif ($type == "pdf") {
+      $html .='<div class="selected-img-preview"><a type="pdf" class="pdf-list select-library-img" id_media="'.$media_opt->id.'" href="'.getMedia($media_opt->name, '150, 150').'"> <i class="mdi mdi-file-pdf"></i> <p class="file_name_pdf">'.$media_opt->name.'</p></a> <div class="img-action"> <button type="button" class="btn btn-success" id="edit-media-img" id_media="'.$media_opt->id.'">Edit</button> <button type="button" class="btn btn-danger" id="delete-media-img" id_media="'.$media_opt->id.'">Delete</button> </div></div>';
+
+    } elseif ($type == "video") {
+      $html .= '<div class="selected-img-preview">';
+      if ($media_opt->type == 'video' && $media_opt->format == 'embed') {
+        $html .='<div class="select-library-img video-selector" id_media="'.$media_opt->id.'"></div>';
+        $html .= $media_opt->name;
+      } elseif ($media_opt->type == 'video' && $media_opt->format != 'embed') {
+        $html .='<div class="select-library-img video-selector" id_media="'.$media_opt->id.'"></div><video width="150" height="135" controls><source src="'.getMedia($media_opt->name, '150, 150').'" type="video/'.$media_opt->format.'"></video>';
+      }
+      $html .= '</div>';
+    }
+
+      return $html;
+}
